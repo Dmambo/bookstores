cac8f5a9c42b79da7c5a26a1871109f704922626
Dmambo <mambotrader0@gmail.com>
2023-01-27 01:16:24 +0530

    Fixed category section

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import List from '../components/List';
 
 const Categories = () => (
-  <div>
-    <List />
-  </div>
+  <button type="button" className="update">
+    Check Status
+  </button>
 );
 
 export default Categories;
