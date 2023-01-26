fc01800ce5b4bea162e58bcbe4a63945090dcb0e
Dmambo <mambotrader0@gmail.com>
2023-01-27 01:09:32 +0530

    Fixed category section

--- src/components/List.js
+++ src/components/List.js
@@ -2,20 +2,9 @@ import React from 'react';
 import '../styles/List.css';
 
 const List = () => (
-  <>
-    <div className="listcategory">
-      <span className="category">Category</span>
-      <select name="category" id="category">
-        <option value="Action">Action</option>
-        <option value="Biography">Biography</option>
-        <option value="History">History</option>
-        <option value="Horror">Horror</option>
-        <option value="Kids">Kids</option>
-        <option value="Learning">Learning</option>
-        <option value="Sci-Fi">Sci-Fi</option>
-      </select>
-    </div>
-  </>
+  <button type="button" className="updated">
+    Check status
+  </button>
 );
 
 export default List;
