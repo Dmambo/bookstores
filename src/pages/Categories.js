fbfc00ee75c8c2aa023e401b0c24668dd8b91e0d
Dmambo <mambotrader0@gmail.com>
2023-01-31 20:27:11 +0530

    Fixed the README file

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import '../styles/Categories.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkStatus } from '../redux/Categories/category';
 
-const Categories = () => (
-  <button type="button" className="updated">
-    Check Status
-  </button>
-);
+const Categories = () => {
+  const selectedCategories = useSelector(
+    // eslint-disable-next-line comma-dangle
+    (state) => state.categories.categories[0]
+  );
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(checkStatus());
+  };
+
+  return (
+    <div className="updated">
+      <h2>{selectedCategories}</h2>
+      <button type="button" onClick={handleClick}>
+        Check status
+      </button>
+    </div>
+  );
+};
 
 export default Categories;
